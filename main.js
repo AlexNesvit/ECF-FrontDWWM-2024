cbb8b1bcb4cdc07ad2445e93625303b76f0fa273
Afpa <alex.nesvit2000@gmail.com>
2024-02-28 09:26:33 +0100

    node.js

--- main.js
+++ main.js
@@ -28,3 +28,22 @@ function validateForm() {
 
     return true;
 }
+
+/*Backend functions 
+npm install -y
+npm install express
+npm start
+npm i -D nodemon*/
+import express from 'express'
+
+const PORT = 5000;
+
+const app = express()
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json("Server is working")
+})
+
+app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))

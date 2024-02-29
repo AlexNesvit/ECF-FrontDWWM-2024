d0c2b066162292cd1370df802b23eef7553e0b13
Afpa <alex.nesvit2000@gmail.com>
2024-02-29 11:03:01 +0100

    back reserv voyage ok

--- app.js
+++ app.js
@@ -64,8 +64,31 @@ app.post('/user', (req,res) => {
   });
 })
 // route pour récupérer le dernier utilisateur enregistré
+app.get('/lastuser', (req, res) => {
+    db.query('SELECT * FROM user ORDER BY iduser DESC LIMIT 1', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
+    });
+});
+// route pour créer un voyage  
+app.post('/travel', (req,res) => {
+    const {user_iduser,trajet_idtrajet,pilote_idpilote,'date voyage':date_voyage} = req.body;
+    
+    const query = 'INSERT INTO `voyage` (`user_iduser`, `trajet_idtrajet`,`pilote_idpilote`, `date voyage`) VALUES (?, ?, ?, ?)';
 
-
+     db.query(query, [user_iduser,trajet_idtrajet,pilote_idpilote,date_voyage], (err, results) => {
+    if (err) {
+      console.error('Erreur lors de l\'insertion des données :', err);
+      res.status(500).json({error:'Erreur lors de l\'insertion des données dans la base de données'});
+      return;
+    }
+   
+    res.status(201).json({message:"Nouveau voyage inséré avec succès !"});
+  });
+})
 
 
 app.listen(port, () => {
--- devFetch.js
+++ devFetch.js
@@ -1,21 +1,27 @@
 const trajetSelect = document.querySelector('#trajet');
 const piloteSelect = document.querySelector('#pilote');
+const formTravel = document.querySelector('#formTravel');
+const travelDatEl = document.querySelector('#date');
+const destinationEl = document.getElementById('trajet');
+const piloteEl = document.getElementById('pilote');
 
 
 document.addEventListener("DOMContentLoaded", () => {
+
     fetch("http://localhost:3000/trajets")
-    .then(response => response.json())
-    .then(response => displayTrajets(response))
-    .catch(error => alert("Erreur : " + error));
+        .then(response => response.json())
+        .then(response => displayTrajets(response))
+        .catch(error => alert("Erreur : " + error));
     fetch("http://localhost:3000/pilotes")
-    .then(response => response.json())
-    .then(response => displayPilotes(response))
-    .catch(error => alert("Erreur : " + error));
+        .then(response => response.json())
+        .then(response => displayPilotes(response))
+        .catch(error => alert("Erreur : " + error));
 });
+
 function displayTrajets(results) {
     results.forEach(result => {
         const option = document.createElement("option");
-        option.value = result['trajet nom'];
+        option.value = result.idtrajet;
         option.textContent = result['trajet nom'];
         trajetSelect.appendChild(option);
     })
@@ -23,8 +29,82 @@ function displayTrajets(results) {
 function displayPilotes(results) {
     results.forEach(result => {
         const option = document.createElement("option");
-        option.value = result['nom pilote'];
+        option.value = result.idpilote;
         option.textContent = result['nom pilote'];
         piloteSelect.appendChild(option);
     })
 }
+async function fetchLastUser() {
+    try {
+        const response = await fetch("http://localhost:3000/lastuser");
+        const data = await response.json(); // Convertit la réponse en JSON
+         // Affiche la réponse dans la console
+        return data; // Retourne les données pour un usage ultérieur
+    } catch (error) {
+        alert("Erreur : " + error);
+    }
+}
+function isRequired(elementValue) {
+    if (elementValue == "") {
+        return false
+    } else {
+        return true
+    }
+}
+function showError(input, message) {
+    const formField = input.parentElement;
+    formField.classList.remove("success")// class css
+    formField.classList.add("error")// class css
+    const errorEl = formField.querySelector("small")
+    errorEl.textContent = message
+}
+// J'ai besoin d'une fonction qui permette d'afficher l'element valide en vert
+function showSuccess(input) {
+    const formField = input.parentElement;
+    formField.classList.remove("error")// class css
+    formField.classList.add("success")// class css
+    const errorEl = formField.querySelector("small")
+    errorEl.textContent = ""
+}
+
+
+formTravel.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    
+        fetchLastUser().then(data => {
+            // Utiliser la variable ici
+            console.log(data)
+            const iduser = data[0].iduser;
+            const user_iduser = iduser.toString();
+            const date_voyage = travelDatEl.value;
+            const trajet_idtrajet = destinationEl.options[destinationEl.selectedIndex].value;
+            
+            const pilote_idpilote = piloteEl.options[piloteEl.selectedIndex].value;
+
+            fetch('http://localhost:3000/travel', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ user_iduser,trajet_idtrajet,pilote_idpilote, 'date voyage': date_voyage})
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok: ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('Success:', data);
+
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        });
+    }
+
+
+
+);
